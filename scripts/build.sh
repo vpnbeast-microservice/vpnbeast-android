b3bd8364af92b9f25979a6693fbf7e6bd970b70c
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-03-06 23:00:09 +0300

    environmental changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,11 +15,11 @@ function git_global_settings() {
 
 function set_release_version() {
     local version_minor
-    version_minor=$(cat version.properties | grep 'VERSION_MINOR' | awk -F '=' '{ print $2 }')
+    version_minor=$(grep 'VERSION_MINOR' version.properties | awk -F '=' '{ print $2 }')
     local version_major
-    version_major=$(cat version.properties | grep 'VERSION_MAJOR' | awk -F '=' '{ print $2 }')
+    version_major=$(grep 'VERSION_MAJOR' version.properties | awk -F '=' '{ print $2 }')
     local version_patch
-    version_patch=$(cat version.properties | grep 'VERSION_PATCH' | awk -F '=' '{ print $2 }')
+    version_patch=$(grep 'VERSION_PATCH' version.properties | awk -F '=' '{ print $2 }')
     echo "${version_major}.${version_minor}.${version_patch}"
 }
 
@@ -29,7 +29,7 @@ function git_commit_and_push() {
     git commit -am "[ci-skip] version ${RELEASE_VERSION}.RELEASE"
     git tag -a "${RELEASE_VERSION}.RELEASE" -m "v${RELEASE_VERSION} tagged"
     git status
-    git push --follow-tags http://${USERNAME}:${GIT_ACCESS_TOKEN}@gitlab.com/vpnbeast/android/vpnbeast-android.git HEAD:${BRANCH}
+    git push --follow-tags http://"${USERNAME}":"${GIT_ACCESS_TOKEN}"@gitlab.com/vpnbeast/android/vpnbeast-android.git HEAD:"${BRANCH}"
 }
 
 BRANCH=master
