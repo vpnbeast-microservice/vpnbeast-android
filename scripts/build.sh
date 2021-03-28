a4bb38b258dd4cc7cbdbf8e9f39c330d6034d4a5
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-03-29 02:22:36 +0300

    fix pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,16 +24,21 @@ function get_release_version() {
 function git_commit_and_push() {
     git --no-pager diff
     git add --all
-    git commit -am "[ci-skip] version ${RELEASE_VERSION}.RELEASE"
-    git tag -a "${RELEASE_VERSION}.RELEASE" -m "v${RELEASE_VERSION} tagged"
+    git commit -am "[ci-skip] version v${RELEASE_VERSION} released"
+    git tag -a "v${RELEASE_VERSION}" -m "v${RELEASE_VERSION} tagged"
     git status
-    git push --follow-tags http://"${USERNAME}":"${GIT_ACCESS_TOKEN}"@gitlab.com/vpnbeast/android/vpnbeast-android.git HEAD:"${BRANCH}"
+    git push --follow-tags "${PUSH_URL}" HEAD:"${BRANCH}"
 }
 
+PROJECT_NAME=vpnbeast-android
+USERNAME=vpnbeast-ci
+EMAIL=info@thevpnbeast.com
 BRANCH=master
 RELEASE_VERSION=$(get_release_version)
 BUILD_METHOD=${1}
 RELEASE_METHOD=${2}
+GIT_ACCESS_TOKEN=${3}
+PUSH_URL=https://${USERNAME}:${GIT_ACCESS_TOKEN}@github.com/vpnbeast/${PROJECT_NAME}.git
 
 gradlew "${BUILD_METHOD}"
 gradlew "${RELEASE_METHOD}"
